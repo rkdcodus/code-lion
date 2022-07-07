de4d1832446221bd0034b633195563757a96c053
rkdcodus <rcd7275@naver.com>
2022-07-07 16:14:12 +0900

    색상 랜덤 선택으로 변경

--- 일단 만드는 자바스크립트/app.js
+++ 일단 만드는 자바스크립트/app.js
@@ -1,6 +1,7 @@
 const lotto = [];
 const lottoArea = document.querySelector(".lottoArea");
 
+
 while(lotto.length < 6 ){
     const num = parseInt(Math.random() * 45 + 1);
     if(lotto.indexOf(num) == -1 ){
@@ -10,6 +11,13 @@ while(lotto.length < 6 ){
 
 lotto.sort((a,b) => a-b);
 
+
 for(var i=0;i<lotto.length;i++){
-    lottoArea.innerHTML += `<div class="lotto">${lotto[i]}</div>`;
+    lottoArea.innerHTML += `<div class="lotto lotto${i}">${lotto[i]}</div>`;    
+}
+
+for(var i=0; i<lotto.length; i++){
+    const colorCode  = "#" + Math.round(Math.random() * 0xffffff).toString(16)
+    // 출처: https://tranquilotter.tistory.com/5 [평온한수달의 평온한이야기:티스토리] 랜덤 색상
+     document.querySelector(`.lotto${i}`).style.backgroundColor = colorCode;
 }

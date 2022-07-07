5a44977cfca805bdee90b8908b3f6b0352152b9e
rkdcodus <rcd7275@naver.com>
2022-07-07 17:58:18 +0900

    자소서 글자 수 제한 실습

--- 일단 만드는 자바스크립트/app.js
+++ 일단 만드는 자바스크립트/app.js
@@ -1,3 +1,5 @@
+// 로또 번호 추첨기
+
 const lotto = [];
 const lottoArea = document.querySelector(".lottoArea");
 
@@ -21,3 +23,16 @@ for(var i=0; i<lotto.length; i++){
     // 출처: https://tranquilotter.tistory.com/5 [평온한수달의 평온한이야기:티스토리] 랜덤 색상
      document.querySelector(`.lotto${i}`).style.backgroundColor = colorCode;
 }
+
+//자기소개서 글자 수 세기
+
+const countTextValue = () => {
+    let content = document.querySelector("#jasoseol").value;  //const로 하면 글자수 제한이 먹히지 않음 const는 재할당 재선언이 불가능. let과 var는 가능. 
+    const contentLength = document.querySelector(".countText");
+    if(content.length > 200) {
+        content = content.substring(0,200);
+        document.getElementById('jasoseol').value = content;
+    }
+    contentLength.innerHTML = `(${content.length}/200)`;
+}
+countTextValue();
